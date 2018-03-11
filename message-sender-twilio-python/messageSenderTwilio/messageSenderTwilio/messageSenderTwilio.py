926830ae0595f33a0ddeb2d048f8908cd745a14b
AymanAlafandy <32833861+AymanAlafandy@users.noreply.github.com>
2018-03-11 23:44:37 +0100

    Add files via upload

--- message-sender-twilio-python/messageSenderTwilio/messageSenderTwilio/messageSenderTwilio.py
+++ message-sender-twilio-python/messageSenderTwilio/messageSenderTwilio/messageSenderTwilio.py
@@ -1,16 +1,17 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = ""
+account_sid = "AC977a41a1943f1d902ef22f9ce3f267d9"
 # Your Auth Token from twilio.com/console 
-auth_token  = ""
+auth_token  = "78e99132a43d4cbf299fe6a5e947b06e"
 
 client = Client(account_sid, auth_token) 
 
+messageToOpen =open("C:\Users\AymanAlafandy\Desktop\ooo.txt")# put a path to your file
 message = client.messages.create(
-    to="+00000000", #telphone number
+    to="+46764279067", #telphone number
     from_="+18313378233", #telephone number 
-    body="Du har en fin mobil. Kan jag låna den?")
-
+    body=messageToOpen.read())
+    
 print(message.sid)
 print 32
